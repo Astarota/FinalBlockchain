a1020d6c55fb3cec6704b3d27f2775492a3ffac6
Astarota <toktamys.dosan@gmail.com>
2022-11-07 16:16:16 +0600

    second-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Navbar, Welcome, Footer, Transactions } from "./components";
 
-import videoBg from "/Users/almicke/project_web3.0/client/images/preview.mp4";
+import videoBg from "./preview.mp4";
 const App = () => (
   <div className="min-h-screen">
     <video className="video" autoPlay loop muted>
